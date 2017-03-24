d4634b66080e0d3f26c06eb87e416bf9971939df
slayercoder <ms81291@gmail.com>
2017-03-24 12:38:24 +0530

    comited the search changes

--- public/js/jquery/script.js
+++ public/js/jquery/script.js
@@ -43,4 +43,14 @@ $("#vegas").vegas({
 //     } 
 // }); 
 
+// (function() {
+//     var cx = '002728072609327069703:s6gkudbut74';
+//     var gcse = document.createElement('script');
+//     gcse.type = 'text/javascript';
+//     gcse.async = true;
+//     gcse.src = 'https://cse.google.com/cse.js?cx=' + cx;
+//     var s = document.getElementsByTagName('script')[0];
+//     s.parentNode.insertBefore(gcse, s);
+//   })();
+
     
