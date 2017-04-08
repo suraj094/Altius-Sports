3cb5c6cec133198aa6fa45ae140d3ab7a4e6b21a
slayercoder <ms81291@gmail.com>
2017-04-08 01:13:27 -0700

    4th update after search page

--- public/js/style.js
+++ public/js/style.js
@@ -3,6 +3,63 @@ $(document).ready(function(){
         e.preventDefault();
         var search =  document.getElementById('keywords').value;
         url = window.location.href.split('search');
-        window.location = 'http://altiussports.com/' + 'search/' +  search;
+        window.location = 'http://148.72.248.114:3000/' + 'search/' +  search;
     })
 });
+
+
+function SUBMIT(){
+var url = window.location.href; 
+  $('#form5').submit(function(e) {
+    var name    = document.getElementById('cont_name');
+    var email   = document.getElementById('cont_email');
+    var ph_no = document.getElementById('cont_number');
+    var message = document.getElementById('cont_message');
+    if (!name.value || !email.value || !message.value) {
+      alert("Fill All Fields !");
+      return false;
+    } else {
+    $.ajax({
+    url: "https://mailthis.to/abhishek.nayyar93@gmail.com", 
+    method: "POST",
+    data: {name: name.value,
+       email: email.value,
+       phone : ph_no.value,
+        message : message.value},
+    dataType: "text/html",
+});
+      e.preventDefault();
+      $(this).get(0).reset();
+      document.getElementById('return-text').innerHTML = "* Thank You! Our program advisor will contact you soon";
+    //   window.location = "http://localhost:3000/products";
+      window.history.go(0);
+    }
+    // window.history.go(0);
+  });
+  
+};
+
+function SUBSCRIBE(){
+var url = window.location.href; 
+  $('#subscribe').submit(function(e) {
+    var email   = document.getElementById('emailId');
+    if (!email.value) {
+      alert("Fill All Fields !");
+      return false;
+    } else {
+    $.ajax({
+    url: "https://mailthis.to/abhishek.nayyar93@gmail.com", 
+    method: "POST",
+    data: {email: email.value},
+    dataType: "text/html",
+});
+      e.preventDefault();
+      $(this).get(0).reset();
+      document.getElementById('return-text').innerHTML = "* Thank You for subscribing our newsletter";
+    //   window.location = "http://localhost:3000/products";
+      window.history.go(0);
+    }
+    // window.history.go(0);
+  });
+  
+};
